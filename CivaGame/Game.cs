a47185ffe92e33b13a4bba8023c33676f0b700c7
Upgade220 <kowalkof@yandex.ru>
2019-11-15 23:52:11 +0500

    new Update 3

--- CivaGame/Game.cs
+++ CivaGame/Game.cs
@@ -1,35 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace CivaGame
 {
     class Game
     {
-        int MapSize;
-        int Score;
-        GameState CurrentState;
+        public int MapSizeX { get; private set; }
+        public int MapSizeY { get; private set; }
+        public int Score { get; private set; }
+        public GameState CurrentState { get; private set; }
         Player Player;
 
-        public void Inicialise()
+        public Game(int x, int y)
         {
+            CurrentState = GameState.Menu;
+            Score = 0;
         }
 
-        public void StartAction()
+        public Point GetPlayerPosition()
         {
+            return new Point(Player.X, Player.Y);
         }
 
-        public void ChangeSrore(int delta)
+        public void StartAction(int x, int y, Point player)
+        {
+            CurrentState = GameState.Action;
+            MapSizeX = x;
+            MapSizeY = y;
+            Player = new Player(player.X, player.Y);
+        }
+
+        public int EndAction()
+        {
+            CurrentState = GameState.Score;
+            return Score;
+        }
+
+        public void ChangeScore(int delta)
         {
             if ((Score + delta) < 0)
                 Score = 0;
             else
                 Score += delta;
-        }
-
-        public void SetGameState (GameState newState)
-        {
-            CurrentState = newState;
         }
     }
 }
