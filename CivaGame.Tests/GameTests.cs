9e2a75f5548c5e88106f393c593f55a9ca35d73d
Slava Zhilyaev <zhilyaev.slava@mail.ru>
2019-11-20 20:20:41 +0500

    finaltests

--- CivaGame.Tests/GameTests.cs
+++ CivaGame.Tests/GameTests.cs
@@ -7,9 +7,41 @@ namespace Tests
     public class GameTests
     {
         [Test]
-        public void omegalul()
+        public void GetplayerPositionTests()
         {
             var game = new Game();
+            game.StartAction();
+            game.GetPlayerPosition().X.Should().BeLessThan(9);
+            game.GetPlayerPosition().X.Should().BeGreaterOrEqualTo(0);
+            game.GetPlayerPosition().Y.Should().BeLessThan(9);
+            game.GetPlayerPosition().Y.Should().BeGreaterOrEqualTo(0);
+        }
+
+        [Test]
+        public void MoveTests()
+        {
+            var game = new Game();
+            game.StartAction();
+            game.Player.X = 8;
+            game.Player.Y = 8;
+            game.Move(Direction.Up);
+            game.Player.Y.Should().Be(0);
+            game.Move(Direction.Right);
+            game.Player.X.Should().Be(0);
+            game.Move(Direction.Down);
+            game.Player.Y.Should().Be(8);
+            game.Move(Direction.Left);
+            game.Player.X.Should().Be(8);
+        }
+
+        [Test]
+        public void PlayerEatTests()
+        {
+            var game = new Game();
+            game.StartAction();
+            game.Player.AddItem(new FoodItem());
+            game.PlayerEat(0);
+            game.Player.InventoryItemsCount[0].Should().Be(0);
         }
     }
 }
--- CivaGame/Trader.cs
+++ CivaGame/Trader.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CivaGame
 {
-    class Trader : ICreature
+    public class Trader : ICreature
     {
         public int X { get; set; }
         public int Y { get; set; }
