ad082d98b6a7c498a47a9848a2e5e6559f6c944b
Slava Zhilyaev <zhilyaev.slava@mail.ru>
2019-11-20 17:18:19 +0500

    playertests1

--- CivaGame.Tests/PlayerTests.cs
+++ CivaGame.Tests/PlayerTests.cs
@@ -7,11 +7,42 @@ namespace Tests
     public class PlayerTests
     {
         [Test]
-        public void BorderTest()
+        public void FoodTests()
         {
             var player = new Player(0, 0, 1);
             player.ChangeFood(-20);
             player.Food.Should().Be(80);
+            player.ChangeFood(-100);
+            player.IsAlive.Should().BeFalse();
+        }
+
+        [Test]
+        public void DamageTests()
+        {
+            var player = new Player(0, 0, 1);
+            player.GetDamage(20);
+            player.IsAlive.Should().BeTrue();
+            player.GetDamage(100);
+            player.IsAlive.Should().BeFalse();
+        }
+
+        [Test]
+        public void HealDamage()
+        {
+            var player = new Player(0, 0, 1);
+            player.GetDamage(20);
+            player.AddItem(new FoodItem());
+            player.UseItem(0, 1);
+            player.HP.Should().Be(100);
+        }
+
+        [Test]
+        public void AddItemToStack()
+        {
+            var player = new Player(0, 0, 1);
+            player.AddItem(new FoodItem);
+            player.AddItem(new FoodItem);
+
         }
     }
 }
